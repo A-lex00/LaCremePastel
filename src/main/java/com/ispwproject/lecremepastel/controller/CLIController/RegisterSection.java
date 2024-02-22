56fa20615153aa42cabcadd1867b0a77708d8f1d
impy07 <imperiamichael01@gmail.com>
2024-02-22 15:57:28 +0100

    cli update

--- src/main/java/com/ispwproject/lecremepastel/controller/CLIController/RegisterSection.java
+++ src/main/java/com/ispwproject/lecremepastel/controller/CLIController/RegisterSection.java
@@ -1,6 +1,7 @@
 package com.ispwproject.lecremepastel.controller.CLIController;
 
 import com.ispwproject.lecremepastel.engineeringclasses.bean.RegisterBean;
+import com.ispwproject.lecremepastel.other.CLIStrings;
 
 import java.util.Scanner;
 
@@ -10,5 +11,21 @@ public class RegisterSection implements CLSection{
         clContext.setSection(this);
         Scanner scanner = new Scanner(System.in);
         RegisterBean registerBean;
+        boolean confirmed = false;
+
+        do{
+            //Select User Type
+            System.out.println(CLIStrings.CHOOSE_USER_TYPE);
+            int userType = Integer.parseInt(scanner.nextLine());
+
+            //Collecting User information
+
+            String confirm;
+//            do {
+//                System.out.println(CLIStrings.CONFIRM_DATA);
+//                confirm = scanner.nextLine();
+//            }while();
+        }while(!confirmed);
+
     }
 }
--- src/main/java/com/ispwproject/lecremepastel/controller/Main.java
+++ src/main/java/com/ispwproject/lecremepastel/controller/Main.java
@@ -2,17 +2,8 @@ package com.ispwproject.lecremepastel.controller;
 
 import com.ispwproject.lecremepastel.controller.CLIController.CLContext;
 import com.ispwproject.lecremepastel.controller.CLIController.StartSection;
-import com.ispwproject.lecremepastel.controller.appcontroller.HelpController;
-import com.ispwproject.lecremepastel.controller.appcontroller.LoginController;
-import com.ispwproject.lecremepastel.controller.appcontroller.ManageUserInfoController;
-import com.ispwproject.lecremepastel.engineeringclasses.bean.LoginBean;
-import com.ispwproject.lecremepastel.engineeringclasses.bean.NoticeBean;
-import com.ispwproject.lecremepastel.engineeringclasses.exception.IncorrectParametersException;
-import com.ispwproject.lecremepastel.engineeringclasses.exception.InvalidSessionException;
 import com.ispwproject.lecremepastel.engineeringclasses.singleton.Configurations;
-import com.ispwproject.lecremepastel.engineeringclasses.singleton.SessionManager;
 import com.ispwproject.lecremepastel.other.CLIStrings;
-import com.ispwproject.lecremepastel.other.NoticeGenerator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,21 +12,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) throws InvalidSessionException, IncorrectParametersException {
+    public static void main(String[] args) {
         String enableGUI = Configurations.getInstance().getProperty("GUI");
 
         //Test area
-        LoginController loginController = new LoginController();
-        String sid = loginController.login(new LoginBean(
-                "tinoC",
-                "1234"
-        ));
-        HelpController helpController = new HelpController();
-        helpController.requestHelp(sid,new NoticeBean(
-                "Non funziona nulla",
-                "Aggiustate pls"
-        ));
-        System.exit(0);
         //End Test area
 
         if(enableGUI.equals("yes")) {
