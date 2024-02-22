1b205cc01579c7b37c2049cdaedbdc061aa6c042
impy07 <imperiamichael01@gmail.com>
2024-02-22 04:52:07 +0100

    ManageProductController Update

--- src/main/java/com/ispwproject/lecremepastel/controller/appcontroller/ManageProductController.java
+++ src/main/java/com/ispwproject/lecremepastel/controller/appcontroller/ManageProductController.java
@@ -1,15 +1,31 @@
 package com.ispwproject.lecremepastel.controller.appcontroller;
 
 import com.ispwproject.lecremepastel.engineeringclasses.bean.ProductBean;
+import com.ispwproject.lecremepastel.engineeringclasses.dao.ProductDAO;
+import com.ispwproject.lecremepastel.engineeringclasses.exception.IncorrectParametersException;
+import com.ispwproject.lecremepastel.model.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ManageProductController {
 
-    public List<ProductBean> loadProducts(){
-        ArrayList<ProductBean> productList = new ArrayList<>();
-        return productList;
+    public List<ProductBean> loadProducts() throws IncorrectParametersException {
+        ArrayList<ProductBean> productBeanList = new ArrayList<>();
+
+        //Load all products available in database
+        ProductDAO productDAO = new ProductDAO();
+        List<Product> productList = productDAO.getAllProducts();
+        for(Product p : productList){
+            productBeanList.add(new ProductBean(
+                    p.getId(),
+                    p.getProductName(),
+                    p.getCategory(),
+                    p.getPrice()
+            ));
+        }
+
+        return productBeanList;
     }
 
 }
