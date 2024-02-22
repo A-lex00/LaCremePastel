f603ff55d8859b5c848406abe799bd9d9504492d
Alessandro Pica <picaalessandro0@gmail.com>
2024-02-22 02:49:38 +0100

    Update MakeOrderController

--- src/main/java/com/ispwproject/lecremepastel/controller/appcontroller/MakeOrderController.java
+++ src/main/java/com/ispwproject/lecremepastel/controller/appcontroller/MakeOrderController.java
@@ -1,6 +1,7 @@
 package com.ispwproject.lecremepastel.controller.appcontroller;
 
 import com.ispwproject.lecremepastel.engineeringclasses.bean.OrderLineBean;
+import com.ispwproject.lecremepastel.engineeringclasses.bean.ProductBean;
 import com.ispwproject.lecremepastel.engineeringclasses.bean.SessionBean;
 import com.ispwproject.lecremepastel.engineeringclasses.dao.OrderLineDAO;
 import com.ispwproject.lecremepastel.engineeringclasses.dao.SimpleOrderDAO;
@@ -8,9 +9,56 @@ import com.ispwproject.lecremepastel.engineeringclasses.exception.IncorrectParam
 import com.ispwproject.lecremepastel.engineeringclasses.exception.InvalidSessionException;
 import com.ispwproject.lecremepastel.engineeringclasses.singleton.SessionManager;
 import com.ispwproject.lecremepastel.model.SimpleOrder;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
+public class MakeOrderController{
+    public class GUIControlloreMakeOrder {
+        private ObservableList currentOrder;
+        private ObservableList orderList;
+        private SessionBean sessionBean;
+        @FXML
+        private ComboBox productBox=new ComboBox<>();
+        @FXML
+        private TextField quantityField=new TextField();
+
+        void inizialize(SessionBean importedSessionBean){
+            this.sessionBean=importedSessionBean;
+        }
+        @FXML
+        void showShoppingCart(ActionEvent showCartEvent) {
+            Node n = (Node) showCartEvent.getSource();
+            Stage shoppingStage = (Stage) n.getScene().getWindow();
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/shoppingCart.fxml"));
+                shoppingStage.setScene(new Scene(root, 629, 481));
+                shoppingStage.setTitle("La Creme Pastel");
+                shoppingStage.show();
+            } catch (Exception e) {
+            }
+        }
+        @FXML
+        void goBack(ActionEvent backEvent){}
+        @FXML
+        void addCart(ActionEvent addEvent){
+
+        }
+
+    }
+
+}
+
+/*
 public class MakeOrderController {
 
 
@@ -20,7 +68,7 @@ public class MakeOrderController {
      * @return The integer value representing the Order ID
      * @throws SQLException If a database error occurs
      * @throws InvalidSessionException If Session ID is not found by SessionManager
-     */
+
   public int createSimpleOrder(String sid) throws SQLException, InvalidSessionException, IncorrectParametersException {
         //Init variables
         SessionBean sessionBean = SessionManager.getInstance().getSession(sid);
@@ -50,7 +98,7 @@ public class MakeOrderController {
      *
      * @param orderLineBean
      * @return true if the operation is executed correctly, false otherwise
-     */
+
     public boolean addOrderLine(OrderLineBean orderLineBean){
         OrderLineDAO orderLineDAO = new OrderLineDAO();
         return orderLineDAO.saveOrderLine(orderLineBean);
@@ -60,9 +108,10 @@ public class MakeOrderController {
      *
      * @param orderLineBean
      * @return true if the operation is executed correctly, false otherwise
-     */
+
     public boolean removeOrderLine(OrderLineBean orderLineBean){
         OrderLineDAO orderLineDAO = new OrderLineDAO();
         return orderLineDAO.deleteOrderLine(orderLineBean);
     }
 }
+*/
