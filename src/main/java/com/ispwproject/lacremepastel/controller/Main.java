32bd8ad89be033d1228c55fcb2efdd6949aab542
impy07 <imperiamichael01@gmail.com>
2024-04-05 12:16:20 +0200

    Merge Commit

--- src/main/java/com/ispwproject/lacremepastel/controller/Main.java
+++ src/main/java/com/ispwproject/lacremepastel/controller/Main.java
@@ -1,10 +1,21 @@
 package com.ispwproject.lacremepastel.controller;
 
-import com.ispwproject.lacremepastel.other.SupportedUserTypes;
+import com.ispwproject.lacremepastel.controller.app.LoginController;
+import com.ispwproject.lacremepastel.engineeringclasses.bean.LoginBean;
+import com.ispwproject.lacremepastel.engineeringclasses.bean.SessionBean;
 
 public class Main {
 
     public static void main(String[] argv){
+
+        LoginBean loginBean = new LoginBean("paolo","");
+        LoginController loginController = new LoginController();
+        SessionBean sessionBean = loginController.login(loginBean);
+        if(sessionBean != null){
+            System.out.println("Login Riuscito");
+        }else{
+            System.out.println("Login Fallito");
+        }
 
     }
 
