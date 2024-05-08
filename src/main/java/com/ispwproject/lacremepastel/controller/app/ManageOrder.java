209e4a4405f79e0d2795b28395d8dd45725ea80c
Alessandro Pica <picaalessandro0@gmail.com>
2024-05-08 11:28:48 +0200

    fix GUI
    remove white board on interface

--- src/main/java/com/ispwproject/lacremepastel/controller/app/ManageOrder.java
+++ src/main/java/com/ispwproject/lacremepastel/controller/app/ManageOrder.java
@@ -1,15 +1,12 @@
 package com.ispwproject.lacremepastel.controller.app;
 
 import com.ispwproject.lacremepastel.engineeringclasses.bean.OrderBean;
-import com.ispwproject.lacremepastel.engineeringclasses.dao.OrderDAO;
 import com.ispwproject.lacremepastel.engineeringclasses.dao.db.OrderDbDAO;
 import com.ispwproject.lacremepastel.engineeringclasses.factory.NoticeFactory;
-import com.ispwproject.lacremepastel.model.Order;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.PropertyResourceBundle;
 
 public class ManageOrder {
     public void finalizeOrder(OrderBean orderBean){
@@ -19,7 +16,7 @@ public class ManageOrder {
         String value=null;
         Properties prop=new Properties();
         try{
-            FileInputStream file= new FileInputStream("config.properties");
+            FileInputStream file= new FileInputStream("src/main/config.properties");
             prop.load(file);
             value=prop.getProperty("PERSISTENCE_TYPE");
             file.close();
