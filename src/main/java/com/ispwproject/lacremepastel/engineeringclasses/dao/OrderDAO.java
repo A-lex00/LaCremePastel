e400ad81ab6ae6c28ee62dec30c6c6c4b382bba0
impy07 <imperiamichael01@gmail.com>
2024-06-04 10:57:47 +0200

    CLI Mega Update

--- src/main/java/com/ispwproject/lacremepastel/engineeringclasses/dao/OrderDAO.java
+++
@@ -1,8 +0,0 @@
-package com.ispwproject.lacremepastel.engineeringclasses.dao;
-
-import com.ispwproject.lacremepastel.engineeringclasses.bean.OrderBean;
-import com.ispwproject.lacremepastel.model.Order;
-
-public interface OrderDAO {
-    boolean saveOrder(OrderBean orderBean);
-}
